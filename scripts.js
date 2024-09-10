d2faa1a1b7d7311ee84a6209a338b60e265ea5ce
Patrick Morgan <pmorgan987@gmail.com>
2024-09-10 00:27:35 -0400

    updating

--- scripts.js
+++ scripts.js
@@ -1,37 +1,53 @@
-const firstNumber = 0;
-const secondNumber = 0;
-const currentOperator = '';
+let firstNumber = 0;
+let currentOperator = "";
+let clearDisplay = false;
 
-
-const number_buttons = document.querySelectorAll('.number');
+const number_buttons = document.querySelectorAll(".number");
 const display = document.querySelector('input[name="number_display"]');
 
 number_buttons.forEach((btn) => {
-    btn.addEventListener('click',() => {
-        const number_value = btn.getAttribute('data-val');
-        updateDisplay(number_value);
-    });
+  btn.addEventListener("click", () => {
+    if (clearDisplay) {
+      display.value = "";
+      clearDisplay = false;
+    }
+    const number_value = btn.getAttribute("data-val");
+    updateDisplay(number_value);
+  });
 });
 
-
 function updateDisplay(val) {
-    if (display.value === '0') display.value = '';
-    display.value += val;
+  if (display.value === "0") display.value = "";
+  display.value += val;
 }
 
+const operator_buttons = document.querySelectorAll(".operation");
+var equation = "";
+operator_buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.getAttribute("data-val") === "+") {
+    } else if (e.target.getAttribute("data-val") === "-") {
+    } else if (e.target.getAttribute("data-val") === "*") {
+    } else if (e.target.getAttribute("data-val") === "/") {
+    } else if (e.target.getAttribute("data-val") === "=") {
+    } else if (e.target.getAttribute("data-val") === "Clear") {
+      firstNumber = 0;
+      currentOperator = "";
+      clearDisplay = false;
+      display.value = "0";
+    }
+  });
+});
 
-const operator_buttons = document.querySelectorAll('.operation');
-var equation = '';
-operator_buttons.forEach(btn => {
-    btn.addEventListener('click',(e) => {
-        if (e.target.getAttribute('data-val') === '+') {
-            alert('plus');
-        } else if (e.target.getAttribute('data-val') === '-') {
-            alert('minus');
-        } else if (e.target.getAttribute('data-val') === '*') {
-            alert('times');
-        } else if (e.target.getAttribute('data-val') === '/') {
-            alert('divide');
-        }
-    });
+const body = document.querySelector("body");
+body.addEventListener("keypress", function (e) {
+  let numberKeys = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  let operatorKeys = new Array("+", "-", "*", "=");
+  if (numberKeys.includes(e.key)) {
+    alert("number pressed");
+  } else if (operatorKeys.includes(e.key)) {
+    alert("operator pressed");
+  } else {
+    alert("we dont care about this key");
+  }
 });
